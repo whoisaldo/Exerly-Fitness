96743e4da80a2554004d58af79555d2db05c5f4f
whoislado <whoisyounes@gmail.com>
2025-06-05 20:08:24 -0400

    updated features

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginSignup.css'
 import user_icon from '../Assets/person.png'
 import email_icon from '../Assets/email.png'
 import password_icon from '../Assets/password.png'
 
 const LoginSignup = () => {
+
+//const [action,setAction] = useState("Login");
+const [action,setAction] = useState("Sign Up");
   return (
     <div className = 'container'>
       <div className="header">
-        <div className = "text">SignUp</div>
+        <div className = "text">{action}</div>
         <div className='underline'></div>
       </div>
       <div className = "subsection">
@@ -32,8 +35,8 @@ const LoginSignup = () => {
         </div>
         <div className="forgot-password">Forgot Password? <span>Click Here!</span></div>
         <div className="submit-container">
-          <div className="submit">Sign up</div>
-          <div className="submit">Login</div>
+          <div className={action === "Login"?"submit gray": "submit"}>Sign up</div>
+          <div className={action === "Sign Up"?"submit gray": "submit"}>Login</div>
         </div>
     </div>
   )
